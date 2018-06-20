028a339b1a5f012fa6abaa6a5c7c9ffd85d4cf3f
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-06-20 14:56:56 -0700

    1.0 fix - Can't check for room on spawn always, so check that elsewhere(now that items deteriorate when outside, not just roofed)

--- Source/MarketValueHPIgnore.cs
+++ Source/MarketValueHPIgnore.cs
@@ -22,24 +22,45 @@ namespace Five_Second_Rule
 	//}
 
 
-	//Just set HP to full if roofed. It will make no difference to anything but value.
+	//Just set HP to full if roofed and roomed. It will make no difference to anything but value.
 	//The rare occasion of a fire breaking out will not destroy as much, but that's too obscure to care about
-	[HarmonyPatch(typeof(Thing))]
-	[HarmonyPatch("SpawnSetup")]
-	public class ThingSpawn_Patch
+	public class RestoreHPToSafeItem
 	{
-		public static void Postfix(Thing __instance)
+		public static void Restore(Thing thing)
 		{
-			ThingDef def = __instance.def;
+			ThingDef def = thing.def;
 
 			//ignore everything that doesn't use StatPart_Health ,
 			//if the thing isn't deteriorating, set back to full hp
 			if (def.useHitPoints && !def.IsApparel && !def.IsCorpse && !def.IsWeapon
-				&& __instance.Position.Roofed(__instance.Map)
-				&& __instance.GetRoom(RegionType.Set_All) is Room room && !room.UsesOutdoorTemperature)
+				&& thing.Position.Roofed(thing.Map)
+				&& thing.GetRoom(RegionType.Set_All) is Room room && !room.UsesOutdoorTemperature)
 			{
-				__instance.HitPoints = __instance.MaxHitPoints;
+				thing.HitPoints = thing.MaxHitPoints;
 			}
+		}
+	}
+
+	[HarmonyPatch(typeof(Thing))]
+	[HarmonyPatch("SpawnSetup")]
+	public class ThingSpawn_Patch
+	{
+		//public override void SpawnSetup(Map map, bool respawningAfterLoad)
+		public static void Postfix(Thing __instance, Map map, bool respawningAfterLoad)
+		{
+			if (map.regionAndRoomUpdater.Enabled)
+				RestoreHPToSafeItem.Restore(__instance);
+		}
+	}
+
+	[HarmonyPatch(typeof(Thing))]
+	[HarmonyPatch("PostMapInit")]
+	public class PostMapInit_Patch
+	{
+		//public virtual void PostMapInit()
+		public static void Postfix(Thing __instance)
+		{
+			RestoreHPToSafeItem.Restore(__instance);
 		}
 	}
 }
