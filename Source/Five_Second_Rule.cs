795fc4008ffc16a53aef9fc9a67912abf2645598
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-09-30 18:58:32 -0700

    suppress DefOf warning on load

--- Source/Five_Second_Rule.cs
+++ Source/Five_Second_Rule.cs
@@ -1,7 +1,8 @@
-﻿	using System.Reflection;
+﻿using System.Reflection;
 using Verse;
 using UnityEngine;
 using Harmony;
+using RimWorld;
 
 namespace Five_Second_Rule
 {
@@ -15,7 +16,18 @@ namespace Five_Second_Rule
 			HarmonyInstance.DEBUG = true;
 #endif
 			HarmonyInstance harmony = HarmonyInstance.Create("Uuugggg.rimworld.Five_Second_Rule.main");
+
+			//Turn off DefOf warning since harmony patches trigger it.
+			harmony.Patch(AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor"),
+				new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
+			
 			harmony.PatchAll();
+		}
+
+		public static bool EnsureInitializedInCtorPrefix()
+		{
+			//No need to display this warning.
+			return false;
 		}
 
 		//public override void DoSettingsWindowContents(Rect inRect)
