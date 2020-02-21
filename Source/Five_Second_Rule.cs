ee83abacf580cfeb85bb1dbcbbb2e5e0ec0e5779
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2020-02-20 17:14:35 -0800

    Preliminary 1.1 update, auto-applied changes

--- Source/Five_Second_Rule.cs
+++ Source/Five_Second_Rule.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using Verse;
 using UnityEngine;
-using Harmony;
+using HarmonyLib;
 using RimWorld;
 
 namespace Five_Second_Rule
@@ -14,9 +14,9 @@ namespace Five_Second_Rule
 			// initialize settings
 			//GetSettings<Settings>();
 #if DEBUG
-			HarmonyInstance.DEBUG = true;
+			Harmony.DEBUG = true;
 #endif
-			HarmonyInstance harmony = HarmonyInstance.Create("Uuugggg.rimworld.Five_Second_Rule.main");
+			Harmony harmony = new Harmony("Uuugggg.rimworld.Five_Second_Rule.main");
 
 			//Turn off DefOf warning since harmony patches trigger it.
 			MethodInfo DefOfHelperInfo = AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor");
